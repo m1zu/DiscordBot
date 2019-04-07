92f6514c8ec7715645528dfd4378e564557e995d
m1zu <m1zu@users.noreply.github.com>
2019-04-07 02:11:31 +0200

    implemented local file argument for token and cmakelis

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -4,7 +4,41 @@
 
 #include "Botclient.h"
 
-int main() {
-	BotClient client("NTU2MjE1OTgwNDk0MjkwOTc3.D22mEg.zCGW1IwEoqS4YPHb69hzYBswtig", 2);
-	client.run();
+#include <iostream>
+#include <fstream>
+#include <exception>
+
+int main(int argc, char** argv) 
+{
+	if (argc != 2)
+	{
+		std::cout << "Usage of executable:\n ./executable  path/to/tokenfile" << std::endl;
+		return -1;
+	}
+
+	try
+	{
+		std::string tokenFileName(argv[1]);
+		std::ifstream tokenFile(tokenFileName, std::ios_base::binary);
+		if (!tokenFile)
+			throw std::runtime_error("Could not load tokenfile: " + tokenFileName);
+
+		std::string token;
+		tokenFile >> token;
+		if (!tokenFile)
+			throw std::runtime_error("Cannot read token from file: " + tokenFileName);
+
+		BotClient client(token, 2);
+		client.run();
+	}
+	catch (std::runtime_error& err)
+	{
+		std::cerr << "Catched runtime exception in main():\n";
+		std::cerr << err.what();
+		return -1;
+	}
+	catch (...)
+	{
+		std::cerr << "Catched some unidentified exception in main()!";
+	}
 }
